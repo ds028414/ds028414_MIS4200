8077ad8cd0c23292f8db96a39f53a07f8fc1e92e
Dakota Smith <ds028414@ohio.edu>
2019-09-11 11:39:41 -0400

    intial model work

---
+++ Models/order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ds028414_MIS4200.Models
+{
+    public class order
+    {
+        public int orderID { get; set; }
+        public int studentID { get; set; }
+        public virtual student student { get; set; }
+        public DateTime orderDate { get; set; }
+    }
+}
---
+++ Models/student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ds028414_MIS4200.Models
+{
+    public class student
+    {
+        public int studentID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public ICollection<order> orders { get; set; }
+    }
+}
